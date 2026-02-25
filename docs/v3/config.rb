46d44004fc0fece8fc98a11a31e95e71719d089d
Johannes Haass <johannes.haass@sap.com>
2026-02-25 08:49:51 +0100

    Modernize docs/v3: Update dependencies, add JavaScript bundler, and implement dark mode (#4869)

--- docs/v3/config.rb
+++ docs/v3/config.rb
@@ -20,8 +20,6 @@ ignore 'includes/api_resources/examples/*.json'
 # Activate the syntax highlighter
 activate :syntax
 
-activate :sprockets
-
 activate :autoprefixer do |config|
   config.browsers = ['last 2 version', 'Firefox ESR']
   config.cascade  = false
